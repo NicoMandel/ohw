f909b614dba62c35028cef6936e507d4a2b3c5a4
Nico Mandel <mandel.nicolas@gmail.com>
2024-09-23 10:50:57 +0200

    included tensorboard logging - model size into naming conventions

--- scripts/test_model.py
+++ scripts/test_model.py
@@ -1,23 +1,27 @@
 import os.path
 from ultralytics import YOLO, settings
 
-def test_model(model : YOLO, data_path):
+def test_model(model : YOLO, data_path,  project : str, name : str):
     metrics = model.val(
         data=data_path,
         imgsz=1280,
-        split="test"
+        split="test",
+        # Naming conventions
+        project=project,
+        name=name
         )
 
     return metrics, model
 
 if __name__=="__main__":
     settings.update({"datasets_dir" : "datasets"})
-    model = YOLO("yolov8s.pt")
+    model_size = "s"
+    model = YOLO("yolov8{}.pt".format(model_size))
     fdir = os.path.dirname(__file__)
     basedir = os.path.abspath(os.path.join(fdir, '..'))
     data_path = os.path.join(basedir, "data", "1cm.yaml")       # https://github.com/ultralytics/ultralytics/issues/8823
 
     # training settings: https://docs.ultralytics.com/modes/train/#train-settings
-    results, model = test_model(model, data_path)
+    results, model = test_model(model, data_path, project="results", name="1cm{}".format(model_size))
     test_model(model)
     print("test debug line")
--- scripts/train_model.py
+++ scripts/train_model.py
@@ -3,29 +3,30 @@ from ultralytics import YOLO, settings
 from test_model import test_model
 
 # https://docs.ultralytics.com/modes/train/#usage-examples
-def train_model(model, data_path):
+def train_model(model : YOLO, data_path, project : str, name : str):
     results = model.train(
         # model = os.path.join(basedir, "yolov8s.pt"),
         # model = "yolov8s.pt",
         data = data_path,
-        epochs=3,
+        epochs=300,
         imgsz=1280,
         batch=2,
         cache=False,
         # naming settings
-        project="results",
-        name="1cm"
+        project=project,
+        name=name
     )
     return results, model
 
 if __name__=="__main__":
     settings.update({"datasets_dir" : "datasets"})
-    model = YOLO("yolov8s.pt")
+    model_size = "s"
+    model = YOLO("yolov8{}.pt".format(model_size))
     fdir = os.path.dirname(__file__)
     basedir = os.path.abspath(os.path.join(fdir, '..'))
     data_path = os.path.join(basedir, "data", "1cm.yaml")       # https://github.com/ultralytics/ultralytics/issues/8823
 
     # training settings: https://docs.ultralytics.com/modes/train/#train-settings
-    results, model = train_model(model, data_path)
-    metrics, model = test_model(model, data_path)
+    results, model = train_model(model, data_path, project="results", name="1cm{}".format(model_size))
+    metrics, model = test_model(model, data_path, project="results", name="1cm{}".format(model_size))
     print("test debug line")
