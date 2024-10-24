6156f84de5cfbeb31c0f511844666d5941070e93
Nico Mandel <mandel.nicolas@gmail.com>
2024-10-24 09:57:40 +0200

    annotate image pulled out as single fn

--- scripts/display_dataset.py
+++ scripts/display_dataset.py
@@ -1,6 +1,7 @@
 import os.path
 from pathlib import Path
 from argparse import ArgumentParser
+import numpy as np
 from ultralytics.utils.plotting import Annotator, colors
 from ohw.dataset import DisplayLabelsDataset
 from ohw.utils import det_to_bb, save_image, get_site_dirs
@@ -13,18 +14,8 @@ def parse_args():
     parser.add_argument("-o", "--output", action="store_true", help="Boolean value. If given, will create output folder structure with <visualisations> subfolder in subdirectory.")
     parser.add_argument("-r", "--recursive", action="store_true", help="Boolean value. If given, will look recursively for subfolders <images> and <labels> and add them to the set.")
     return parser.parse_args()
-    
-def visualise_images(input_dir : str, output : bool):
-    site_dir = os.path.abspath(input_dir)
-    visdir = Path(os.path.abspath(os.path.join(site_dir, "visualisations")))
-    yds = DisplayLabelsDataset(site_dir)
 
-    # if --output is given
-    if output:
-        visdir.mkdir(exist_ok=True)
-        print("Created: {}\nWill write images to it, if they don't exist yet".format(visdir))
-        
-    for img, detections, img_id in tqdm(yds, leave=True):
+def annotate_image(img : np.ndarray, detections : np.ndarray) -> np.ndarray:
         # https://docs.ultralytics.com/usage/simple-utilities/#horizontal-bounding-boxes
         ann = Annotator(
             img,
@@ -39,6 +30,20 @@ def visualise_images(input_dir : str, output : bool):
             label = "OHW"
             ann.box_label(box, label, colors(c_idx, bgr=False))
         img_w_bboxes = ann.result()
+        return img_w_bboxes
+
+def visualise_images(input_dir : str, output : bool):
+    site_dir = os.path.abspath(input_dir)
+    visdir = Path(os.path.abspath(os.path.join(site_dir, "visualisations")))
+    yds = DisplayLabelsDataset(site_dir)
+
+    # if --output is given
+    if output:
+        visdir.mkdir(exist_ok=True)
+        print("Created: {}\nWill write images to it, if they don't exist yet".format(visdir))
+        
+    for img, detections, img_id in tqdm(yds, leave=True):
+        img_w_bboxes = annotate_image(img, detections)
         if output:
             out_p = visdir / (img_id + ".jpg")
             save_image(img_w_bboxes, str(out_p))
