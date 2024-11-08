f4b3124f565a3beef640b19f2f1d35ed29562b90
Nico Mandel <mandel.nicolas@gmail.com>
2024-11-08 16:57:22 +0100

    pwd also added for training local sing

--- scripts/singularity_train.sh
+++ scripts/singularity_train.sh
@@ -14,8 +14,9 @@ basedir="/media/nico/sdcReplica"
 #         --bind /mnt/scratch_lustre/hawkweed_drone_scratch/saves:/home/ubuntu/results \
 #         "$basedir/scratch_lustre/yolo.simg" python3 -u train.py     # actual training container
 
-singularity exec --bind "$basedir/appsource/local/hawkweed_drone/ohw/scripts":/home/ubuntu/ \
+# bind order for directories is important
+singularity exec --bind --pwd /home/ubuntu "$basedir/appsource/local/hawkweed_drone/ohw/scripts":/home/ubuntu/ \
         --bind "$basedir/appsource/local/hawkweed_drone/ohw/src/ohw":/home/ubuntu/ohw \
         --bind "$basedir/scratch_lustre/datasets/base":/home/ubuntu/datasets \
         --bind "$basedir/scratch_lustre/results/":/home/ubuntu/results \
-        "$basedir/scratch_lustre/yolo-rawpy.simg" python3 /home/ubuntu/train_model.py s /home/ubuntu/datasets/1cm/1cm.yaml 
+        "$basedir/scratch_lustre/yolo-rawpy.simg" python3 train_model.py s datasets/1cm/1cm.yaml 
