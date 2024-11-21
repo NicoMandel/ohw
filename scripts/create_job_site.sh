ca26e05196b67350f4abe9a1da5c5fbe0d5760d3
Nico Mandel <mandel.nicolas@gmail.com>
2024-11-21 14:11:25 +0100

    directory binding for inference

--- scripts/create_job_site.sh
+++ scripts/create_job_site.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# code and data reposiories
+code_repo="/home/mandeln/ohw"
+base_shared_dir="/mnt/scratch_lustre/hawkweed_drone_scratch"
+
 # Start by changing this
-site_location="~/src/csu/OHW_data/SDC_Sites/2312122_PP"
+site_location="/mnt/load/aerial/drone_uav/2023-24/NPWS_OHW_4/23.12.18 - Yarrabee North"
 # site_location="/home/nico/src/csu/OHW_data/SDC_Sites/2312122_PP"
+
 # automated job naming and finding subdirectories
 sitename=$(basename "$site_location")
 mapfile -t flightdirs < <(find "$site_location" -maxdepth 1 -type d -iname "flight*")
 
+module load slurm
 # echo all the relevant factors into the base file by structure
 for jobsite in "${flightdirs[@]}"; do
     # create job name
@@ -23,21 +29,22 @@ for jobsite in "${flightdirs[@]}"; do
     echo "#SBATCH --gpus-per-node=1" >> "$jn".sh
     echo "#SBATCH --mem 32G" >> "$jn".sh
     echo "#SBATCH -t 0-03:59" >> "$jn".sh
-    echo "#SBATCH --job-name=$jn" >> "$jn".sh
-    echo "#SBATCH --err=/mnt/scratch_lustre/hawkweed_drone_scratch/log_nico-job-%j.err" >> "$jn".sh
-    echo "#SBATCH --output=/mnt/scratch_lustre/hawkweed_drone_scratch/log_nico/job-%j.out" >> "$jn".sh
+    echo "#SBATCH --job-name=\"$jn\"" >> "$jn".sh
+    echo "#SBATCH --err=$base_shared_dir/log_nico/job-%j.err" >> "$jn".sh
+    echo "#SBATCH --output=$base_shared_dir/log_nico/job-%j.out" >> "$jn".sh
 
     # module parts
     echo "module purge" >> "$jn".sh
     echo "module load go singularity" >> "$jn".sh
 
     # actual job - ensure that the directories are correct - input and output!
-    echo "singularity exec --nv --pwd /home/ubuntu --bind /home/mandeln/ohw/scripts:/home/ubuntu/ \
-            --bind /home/mandeln/ohw/src/ohw:/home/ubuntu/ohw \
-            --bind /mnt/scratch_lustre/hawkweed_drone_scratch/data_nico/inference:/home/ubuntu/inference \
-            --bind /mnt/scratch_lustre/hawkweed_drone_scratch/results_nico:/home/ubuntu/results \
-            /mnt/scratch_lustre/hawkweed_drone_scratch/yolo-rawpy.simg python3 -u inference_sahi.py \
-            $jobsite results/20241109-n-1cm-1cm/weights/best.pt inference -n \"$sitename/$jobname\" -s -v" >> "$jn".sh
+    echo "singularity exec --nv --pwd /home/ubuntu --bind $code_repo/scripts:/home/ubuntu/ \
+            --bind $code_repo/src/ohw:/home/ubuntu/ohw \
+            --bind \"$jobsite\":/home/ubuntu/inference \
+            --bind $base_shared_dir/inference_out:/home/ubuntu/inference_out \
+            --bind $base_shared_dir/results_nico:/home/ubuntu/results \
+            $base_shared_dir/yolo-rawpy.simg python3 -u inference_sahi.py \
+            inference results/20241109-n-1cm-1cm/weights/best.pt inference_out -n \"$sitename/$jobname\" -s -v" >> "$jn".sh
 
     # choose between sbatch "$jn.sh" or cat "$jn.sh"
     sbatch "$jn.sh"
