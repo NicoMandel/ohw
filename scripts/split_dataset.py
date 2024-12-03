6417612e8c3ee4d4bf1cd91c43eab5508d851324
Nico Mandel <mandel.nicolas@gmail.com>
2024-12-03 11:25:56 +0100

    split dataset - argparse and file loc

--- scripts/split_dataset.py
+++ scripts/split_dataset.py
@@ -2,21 +2,19 @@
 # auto-labelling-> https://docs.ultralytics.com/usage/simple-utilities/?h=sam+labeling
 
 import os.path
+from argparse import ArgumentParser
 from ultralytics.data.utils import autosplit
 
+def parse_args():
+    parser = ArgumentParser(description="Script for generating new splits, after new data has been added to dataset.")
+    parser.add_argument("input", type=str, help="Location of the dataset image folder to be split. **Excluding** the <train> part in the end, for correct file placement.")
+    return parser.parse_args()
 
 if __name__=="__main__":
-    fdir = os.path.dirname(__file__)
-    basedir = os.path.abspath(os.path.join(fdir, '..'))
-
-    ds_name = "0.24cm"
-    dataset_path = os.path.join(basedir, "datasets", ds_name, "train")
+    args = parse_args()
 
     autosplit(
-        path=dataset_path,
+        path=args.input,
         weights=(0.8, 0.2, 0.0),
         annotated_only=False
     )
-
-    # print("Test Debug line")
-    
