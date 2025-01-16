bf204fba0935d3a399f31b99f8926de2b050a54a
Nico Mandel <mandel.nicolas@gmail.com>
2025-01-16 19:05:52 +0100

    pathlib parent resolution

--- scripts/inference_sahi.py
+++ scripts/inference_sahi.py
@@ -40,7 +40,7 @@ def sahi(input_dir : str, registry_f : str, resolution : str, output : str, name
         pixel : int = 50, lw : int =5):
     # Model
     model_name, conf_thresh = get_model(registry_f, resolution, confidence, metric)
-    model_prfx = os.path.dirname(registry_f) if registry_f.endswith(".xlsx") else os.path.dirname(os.path.dirname(registry_f))
+    model_prfx = str(Path(registry_f).resolve().parent)
     model_p = os.path.join(model_prfx , model_name, 'weights', 'best.pt')
         
     # overwrite confidence, if the arg is given 
